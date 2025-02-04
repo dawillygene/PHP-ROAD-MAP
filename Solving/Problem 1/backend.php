f4842706a8ed077f437ceb4d17ebe498b29d8064
dawillygene <eliamariki2000@gmail.com>
2025-02-04 08:57:01 +0300

    solving problem 1

---
+++ Solving/Problem 1/backend.php
@@ -0,0 +1,57 @@
+<?php 
+
+function connection(){
+    $server = "localhost";
+    $username = "dawilly";
+    $password = "babalaolau";
+    $db = "URL";
+
+    $conn = new mysqli($server,$username,$password);
+    if($conn->connect_error){
+        return "error in connection to databas";
+    }
+    $conn->select_db($db) or die("Cant select database");
+
+    return $conn;
+}
+
+
+if($_SERVER['REQUEST_METHOD']==="POST"){
+
+    $conn = connection();
+
+    $url =$conn->real_escape_string(trim($_POST['url']));
+    $desc =$conn->real_escape_string(trim($_POST['desc']));
+    $sql = "INSERT INTO Urltable(url,des) values('$url','$desc')";
+    if($conn->query($sql)){
+        echo "data inserted successfully";
+    }else{
+        echo "error occured during data insertion";
+    }
+
+$conn->close();
+
+}
+
+$conn = connection();
+$sql = "select * from Urltable ORDER BY id desc";
+$result =$conn->query($sql);
+if($result->num_rows > 0){
+echo "<table>
+<thead>
+<th>URL</th>
+<th>description</th>
+</thead>
+<tbody>
+";
+    while($row = $result->fetch_assoc()){
+     echo "<tr> 
+     <td>" .$row['url'] ."</td>
+     <td>" .$row['des'] ."</td>
+     </tr>";    
+    }
+    echo "</tbody>";
+    echo "</table>";
+
+}
+?>
---
+++ Solving/Problem 1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URL AND DESCRIPTION SITE</title>
+</head>
+<body>
+    
+<form action="backend.php" method="post">
+<label for="">URL</label> <br>
+<input type="text" name="url" placeholder="Enter the URL"><br>
+<label for="">DESCRIPTION</label>
+<textarea name="desc"></textarea>
+<input type="submit" name="" value="save">
+</form>
+</body>
+</html>
